88935feb0fe1b06b4b05750ed190617c43d8f9ac
bccbass <109984363+bccbass@users.noreply.github.com>
2024-08-30 17:07:36 +1000

    tried altering vid playback with useRef. still wip

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -12,7 +12,7 @@ const Section = ({ section, children }) => {
       <div className="w-full max-w-3xl flex flex-col items-center">
         <div className=" flex flex-col my-3 w-full ">
           <div className={section.img && "my-4"}>
-            <h2 className="bg-red-400 text-neutral-200  px-3 py-3 text-2xl text-end rounded-tl-3xl border-b border-red-600">
+            <h2 className="bg-red-400 text-neutral-200  px-3 py-3 text-2xl text-end rounded-tl-3xl ">
               {section.heading}
             </h2>
             {section.img && (
--- src/components/VidPlayer.jsx
+++ src/components/VidPlayer.jsx
@@ -1,32 +1,31 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const VidPlayer = ({project, activeProject}) => {
+  const vidRef = useRef(null)
     const [isPlaying, setIsPlaying] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
-  const handleClick = () => setIsPlaying(project.videoID);
+    const handlePlayVideo = () => {
+      setIsPlaying(project.videoID)
+      vidRef.current.play()
+    }
+  // const handleClick = () => setIsPlaying(project.videoID);
 
   return (
     <div className="">
-        {activeProject != project.videoID ? (
-            < div >
-            {isLoading && <h1>LOADING</h1>}
-          <img
-            onClick={handleClick}
-            className="rounded"
-            src={project.img}
-            alt=""
-          />
-          </div>
-        ) : (
+        {
+       (
           <div className="w-full">
                 <video
+                ref={vidRef}
+                // onClick={handlePlayVideo}
     className="w-full"
     height={"100%"}
     width={"100%"}
+    // autoPlay={isPlaying}
+
     controls
-    autoPlay
-    poster={project.IMG}
+    poster={project.img}
     playsInline
     src={`https://res.cloudinary.com/dyb9ascpy/video/upload/v1724886027/possumpark/video/${project.videoID}.mov`}
     // src={`https://res.cloudinary.com/dyb9ascpy/video/upload/f_auto:video,q_auto/v1724886027/possumpark/video/${videoID}.mov`}
