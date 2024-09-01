95712b5184189e5fa9cca442332c6877f9327a16
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2024-09-01 14:12:14 +1000

    added ref to vids

--- src/components/VidPlayer.jsx
+++ src/components/VidPlayer.jsx
@@ -2,21 +2,28 @@ import React from "react";
 import { useState, useRef } from "react";
 
 const VidPlayer = ({ project, isPlaying, setIsPlaying }) => {
+  const vidRef = useRef(null)
   return (
     <div className="">
       {
         <div className="w-full h-1/2">
           {isPlaying == project.videoID ? (
+            <div className="">
             <video
+            ref={vidRef}
               className="w-full"
-              height={"100%"}
+              // height={"100%"}
               width={"100%"}
               autoPlay
               controls
-              // poster={project.img}
+              poster={project.img}
               playsInline
               src={`https://res.cloudinary.com/dyb9ascpy/video/upload/v1724886027/possumpark/video/${project.videoID}.mov`}
+              type="video/mp4"
             ></video>
+            <div onClick={() => vidRef.current.play()}>play</div>
+            <div onClick={() => vidRef.current.pause()}>pause</div>
+            </ div>
           ) : (
             <img
               onClick={() => setIsPlaying(project.videoID)}
