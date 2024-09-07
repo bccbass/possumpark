bf835bc6294731509865a0f56ba53fcd60a4665c
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2024-09-07 16:13:20 +1000

    adjusted html body to try to prevent dragging on mobile. Style edits to whatwedo

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -14,22 +14,20 @@ const WhatWeDo = () => {
 
         <div className="julius-sans-one-regular bg-red-500 p-2 flex flex-col items-center justify-center">
           <div className="julius-sans-one-regular bg-red-500 text-neutral-100 w-full p-6 border-2 border-neutral-100 text-justify flex flex-col items-center justify-center">
-            <div className="text-center text-2xl">
-              Services Offered
-            </div>
+            <div className="text-center text-2xl">What We Do</div>
             <div className=" w-1/2 sm:w-3/5  h-1 border-b-2 border-neutral-100 rounded-sm  my-6 "></div>
 
-            <ul className="text-center text-xl sm:text-2xl flex flex-col space-y-8 sm:space-y-9">
+            <ul className="text-center text-xl sm:text-2xl flex flex-col space-y-8 sm:space-y-10">
               <li>Mixing</li>
-              <li>Podcasts</li>
-              <li>Instruction in production and working in daws</li>
+              <li>Lessons in production & working in daws</li>
               <li>World class session musicians</li>
-              <li>Backing tracks for artists and musical theatre</li>
+              <li>Backing tracks for artists & bands</li>
+              <li>Backing tracks for musical theatre</li>
 
-              <li>Music for Film and Television</li>
+              <li>Music for Film & Television</li>
               <li>Music for Commercials</li>
-              <li>Audiobooks</li>
-              <li>Post production and Sound Design</li>
+              <li>Podcasts & Audiobooks</li>
+              <li>Post production & Sound Design</li>
             </ul>
             <div className=" w-1/2 sm:w-3/5  h-1 border-b-2 border-neutral-100 rounded-sm  my-6"></div>
           </div>
